db6fa28a6dfda5811cfad936333c726f8655da7f
Kouji Matsui <k@kekyo.net>
2021-02-10 13:29:53 +0900

    Added Xamarin.Forms sample (WIP)

---
+++ samples/EpoxyHello.Xamarin.Forms/EpoxyHello.Xamarin.Forms/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Epoxy - A minimum MVVM assister library.
+// Copyright (c) 2019-2021 Kouji Matsui (@kozy_kekyo, @kekyo2)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using Xamarin.Forms;
+
+namespace EpoxyHello.Xamarin.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
