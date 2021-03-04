88653625740ae6c8458c3e6c6822816000c25db7
Kouji Matsui <k@kekyo.net>
2021-03-04 15:08:58 +0900

    Fixed failure building Xamarin Forms sample.

--- samples/EpoxyHello.Xamarin.Forms/EpoxyHello.Xamarin.Forms.Android/MainActivity.cs
+++ samples/EpoxyHello.Xamarin.Forms/EpoxyHello.Xamarin.Forms.Android/MainActivity.cs
@@ -17,14 +17,12 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-using System;
+#nullable enable
 
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace EpoxyHello.Xamarin.Forms.Droid
 {
@@ -42,7 +40,8 @@ namespace EpoxyHello.Xamarin.Forms.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(
+            int requestCode, string[]? permissions, [GeneratedEnum] Android.Content.PM.Permission[]? grantResults)
         {
             global::Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
--- samples/EpoxyHello.Xamarin.Forms/EpoxyHello.Xamarin.Forms.UWP/App.xaml.cs
+++ samples/EpoxyHello.Xamarin.Forms/EpoxyHello.Xamarin.Forms.UWP/App.xaml.cs
@@ -55,7 +55,7 @@ namespace EpoxyHello.Xamarin.Forms.UWP
             }
 #endif
 
-            Frame rootFrame = Window.Current.Content as Frame;
+            var rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
