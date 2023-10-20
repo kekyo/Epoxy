e7b570f247d24fde637b9320a88d9c0cf27d9c9d
Kouji Matsui <k@kekyo.net>
2023-10-20 10:11:12 +0900

    Fixed template code fragments (WIP)

--- templates/templates/EpoxyHello.Xamarin.Forms/EpoxyHello.Android/MainActivity.cs
+++ templates/templates/EpoxyHello.Xamarin.Forms/EpoxyHello.Android/MainActivity.cs
@@ -3,34 +3,35 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 
-namespace EpoxyHello;
-
-[Activity(
-    Label = "EpoxyHello",
-    Icon = "@mipmap/icon",
-    Theme = "@style/MainTheme",
-    MainLauncher = true,
-    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-public sealed class MainActivity :
-    global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+namespace EpoxyHello
 {
-    protected override void OnCreate(Bundle savedInstanceState)
+    [Activity(
+        Label = "EpoxyHello",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public sealed class MainActivity :
+        global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        TabLayoutResource = Resource.Layout.Tabbar;
-        ToolbarResource = Resource.Layout.Toolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
 
-        base.OnCreate(savedInstanceState);
+            base.OnCreate(savedInstanceState);
 
-        global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-        global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
-        LoadApplication(new App());
-    }
-    public override void OnRequestPermissionsResult(
-        int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-    {
-        global::Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(
+            int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            global::Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
