cc2a287e3d2ee889feac8c28afd45502696303a1
Kouji Matsui <k@kekyo.net>
2024-03-06 14:26:01 +0900

    Re-enabled Maui.

---
+++ playground/EpoxyHello.Maui/Platforms/Android/MainApplication.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace EpoxyHello.Maui;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
